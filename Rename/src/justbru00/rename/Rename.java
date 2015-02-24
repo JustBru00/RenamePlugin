681731265f9774914380418a1bfd5506cc6faa76
Justin Brubaker <justbru00@gmail.com>
2015-02-23 19:12:14 -0500

    Changed Comment Type at the top of *.java files.

--- Rename/src/justbru00/rename/Rename.java
+++ Rename/src/justbru00/rename/Rename.java
@@ -14,30 +14,34 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-//******************************************
-//DO NOT COPY WITHOUT PERMISSION.
-//EpicRename Plugin:
-//This plugin renames the current held item.
-//Look at the comments.
-//Author: Justin Brubaker aka. JustBru00.
-//Date: 2/11/2015
-//
-//Copyright (C) 2015 Justin Brubaker
-//
-//This program is free software; you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation; either version 2 of the License, or
-//(at your option) any later version.
-//
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License along
-//with this program; if not, write to the Free Software Foundation, Inc.,
-//51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//*******************************************
+/* 
+ *******************************************
+*DO NOT COPY WITHOUT PERMISSION.
+*EpicRename Plugin:
+*This plugin renames the current held item.
+*Look at the comments.
+*Author: Justin Brubaker aka. JustBru00.
+*Date: 2/11/2015
+*
+*Copyright (C) 2015 Justin Brubaker
+*
+*This program is free software; you can redistribute it and/or modify
+*it under the terms of the GNU General Public License as published by
+*the Free Software Foundation; either version 2 of the License, or
+*(at your option) any later version.
+*
+*This program is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU General Public License for more details.
+*
+*You should have received a copy of the GNU General Public License along
+*with this program; if not, write to the Free Software Foundation, Inc.,
+*51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+* *******************************************
+*/ 
+ 
+
 
 // Class declaration 
 public class Rename extends JavaPlugin {
