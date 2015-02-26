dc6deb616655b3f7810098f03be4f1cd0cf460fe
Justin Brubaker <justbru00@gmail.com>
2015-02-26 11:14:48 -0500

    Prepare more config and remove some comments

--- Rename/src/justbru00/rename/Rename.java
+++ Rename/src/justbru00/rename/Rename.java
@@ -43,134 +43,98 @@ import org.bukkit.plugin.java.JavaPlugin;
  
 
 
-// Class declaration 
+
 public class Rename extends JavaPlugin {
-	// Console Messages
+	
 	public final Logger logger = Logger.getLogger("Minecraft");
 	
-		
-
-	// On Command
 	@Override
 	public boolean onCommand(CommandSender sender, Command command,
 			String commandLabel, String[] args) {
 		Player player = (Player) sender;
-
-		// if CommandLabel is rename (/rename)
+		
+		
 		if (commandLabel.equalsIgnoreCase("rename")) {
-			if (sender.hasPermission(new Permissions().rename)) {
-				// if args is length 1
-				if (args.length == 1) {
-					// Non-Colored Text
+			if (getConfig().getInt("Config Verison") == 2) {
+			if (sender.hasPermission(new Permissions().rename)) {				
+				if (args.length == 1) {				
 					String nonColoredText = args[0];
-					// Colored Text
 					String coloredText = ChatColor
-							.translateAlternateColorCodes('&', nonColoredText);
-					// PlayerInventory (pi) Used later
-					PlayerInventory pi = player.getInventory();
-					// ItemStack inHand gets what player is holding (again used
-					// later)
-					ItemStack inHand = pi.getItemInHand();
-
-					// Check if the player is not holding Air (nothing)
-					if (player.getItemInHand().getType() != Material.AIR) {
-						// Check if the player is holding a diamond pick
-						if (player.getItemInHand().getType() == Material.DIAMOND_PICKAXE) {
-							// Make a new ItemStack that is the type of what
-							// they are holding
-							ItemStack newitem = new ItemStack(inHand);
-							// Make a new ItemMeta (im) that = What the player
-							// is holdings ItemMeta
-							ItemMeta im = inHand.getItemMeta();
-							// set (im) to coloredText (What the user typed in
-							// /rename
-							im.setDisplayName(coloredText);
-							// Set newitems Meta to (im)
-							newitem.setItemMeta(im);
-							// Get rid of the Item the player is holding
-							pi.removeItem(inHand);
-							// Set what they are holding to be our new item
-							pi.setItemInHand(newitem);
-							// Print to console
+							.translateAlternateColorCodes('&', nonColoredText);	
+					
+					PlayerInventory pi = player.getInventory();					
+					ItemStack inHand = pi.getItemInHand();	
+					
+					if (player.getItemInHand().getType() != Material.AIR) {						
+						if (player.getItemInHand().getType() == Material.DIAMOND_PICKAXE) {	
+							
+							ItemStack newitem = new ItemStack(inHand);							
+							ItemMeta im = inHand.getItemMeta();							
+							im.setDisplayName(coloredText);							
+							newitem.setItemMeta(im);							
+							pi.removeItem(inHand);						
+							pi.setItemInHand(newitem);						
 							this.logger.info(player.getName()
-									+ " renamed a(n) item to: " + coloredText);
-							// Tell the player renamed
+									+ " renamed a(n) item to: " + coloredText);							
 							player.sendMessage(ChatColor.GREEN
 									+ "Item renamed.");
 						} else
-							// if player isn't holding a diamond pick
+							
 							player.sendMessage(ChatColor.RED
 									+ "You can only rename a diamondpickaxe.");
 					} else
-						// if the player is holding AIR (nothing)
-						player.sendMessage(ChatColor.RED
+										player.sendMessage(ChatColor.RED
 								+ "The Item in hand is air.");
 
-				} else
-					// if player types /rename with no text after it or player
-					// types to many word like: /rename hi hi hello.
-					player.sendMessage(ChatColor.RED
-							+ "Please only put 1 word. Like this: &bT&6E&5S&1T or TEST");
+				} else {
+	                   String error2 = getConfig().getString("not_enough_or_too_many_args");
+				
+				
+						  player.sendMessage(ChatColor.translateAlternateColorCodes('&', error2));
+						}
 			} else {
 				String error1 = getConfig().getString("no_permission");
 			player.sendMessage(ChatColor.translateAlternateColorCodes('&', error1));
 			}
+		} else this.logger.warning("EpicRename: Your config is out dated please delete it and restart the server!!!!!");
 		}
 		if (commandLabel.equalsIgnoreCase("renameany")) {
-			if (sender.hasPermission(new Permissions().renameany)) {
-				// if args is length 1
-				if (args.length == 1) {
-					// Non-Colored Text
-					String nonColoredText2 = args[0];
-					// Colored Text
+			if (sender.hasPermission(new Permissions().renameany)) {			
+				if (args.length == 1) {			
+					String nonColoredText2 = args[0];				
 					String coloredText2 = ChatColor
-							.translateAlternateColorCodes('&', nonColoredText2);
-					// PlayerInventory (pi2) Used later
-					PlayerInventory pi2 = player.getInventory();
-					// ItemStack inHand2 gets what player is holding (again used
-					// later)
-					ItemStack inHand2 = pi2.getItemInHand();
-
-					// Check if the player is not holding Air (nothing)
-					if (player.getItemInHand().getType() != Material.AIR) {
-						// Make a new ItemStack that is the type of what
-						// they are holding
-						ItemStack newitem2 = new ItemStack(inHand2);
-						// Make a new ItemMeta (im2) that = What the player
-						// is holdings ItemMeta
-						ItemMeta im2 = inHand2.getItemMeta();
-						// set (im2) to coloredText (What the user typed in
-						// /rename
-						im2.setDisplayName(coloredText2);
-						// Set newitems Meta to (im2)
-						newitem2.setItemMeta(im2);
-						// Get rid of the Item the player is holding
-						pi2.removeItem(inHand2);
-						// Set what they are holding to be our new item
-						pi2.setItemInHand(newitem2);
-						// Print to console
+							.translateAlternateColorCodes('&', nonColoredText2);				
+					PlayerInventory pi2 = player.getInventory();			
+					ItemStack inHand2 = pi2.getItemInHand();			
+					if (player.getItemInHand().getType() != Material.AIR) {						
+						ItemStack newitem2 = new ItemStack(inHand2);	
+						ItemMeta im2 = inHand2.getItemMeta();						
+						im2.setDisplayName(coloredText2);						
+						newitem2.setItemMeta(im2);			
+						pi2.removeItem(inHand2);			
+						pi2.setItemInHand(newitem2);						
 						this.logger.info(player.getName()
-								+ " renamed a(n) item to: " + coloredText2);
-						// Tell the player renamed
+								+ " renamed a(n) item to: " + coloredText2);						
 						player.sendMessage(ChatColor.GREEN + "Item renamed.");
 
-					} else
-						// if the player is holding AIR (nothing)
+					} else {						
 						player.sendMessage(ChatColor.RED
 								+ "The Item in hand is air.");
-
-				} else
-					// if player types /rename with no text after it or player
-					// types to many word like: /rename hi hi hello.
-					player.sendMessage(ChatColor.RED
-							+ "Please only put 1 word. Like this: &bT&6E&5S&1T or TEST");
+					}
+				} else {
+                   String error2 = getConfig().getString("not_enough_or_too_many_args");
+		
+				
+				  player.sendMessage(ChatColor.translateAlternateColorCodes('&', error2));
+				}
 			} else {
 				String error1 = getConfig().getString("no_permission");
 		
 				
 				player.sendMessage(ChatColor.translateAlternateColorCodes('&', error1));
 			}
-		}
+		}		 
+		
 		return false;
 
 	}
