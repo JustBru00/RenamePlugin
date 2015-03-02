92ab6b8b1cb3ac1d656667b0f7c7c254e2c4fbb6
Justin Brubaker <justbru00@gmail.com>
2015-03-02 16:38:54 -0500

    More Config stuff :D

--- Rename/src/justbru00/rename/Rename.java
+++ Rename/src/justbru00/rename/Rename.java
@@ -1,6 +1,5 @@
 package justbru00.rename;
 
-
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
@@ -16,120 +15,128 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  *******************************************
-*DO NOT COPY WITHOUT PERMISSION.
-*EpicRename Plugin:
-*This plugin renames the current held item.
-*Look at the comments.
-*Author: Justin Brubaker aka. JustBru00.
-*Date: 2/11/2015
-*
-*Copyright (C) 2015 Justin Brubaker
-*
-*This program is free software; you can redistribute it and/or modify
-*it under the terms of the GNU General Public License as published by
-*the Free Software Foundation; either version 2 of the License, or
-*(at your option) any later version.
-*
-*This program is distributed in the hope that it will be useful,
-*but WITHOUT ANY WARRANTY; without even the implied warranty of
-*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*GNU General Public License for more details.
-*
-*You should have received a copy of the GNU General Public License along
-*with this program; if not, write to the Free Software Foundation, Inc.,
-*51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-* *******************************************
-*/ 
- 
-
-
+ * DO NOT COPY WITHOUT PERMISSION. EpicRename Plugin: This plugin renames the
+ * current held item. Look at the comments. Author: Justin Brubaker aka.
+ * JustBru00. Date: 2/11/2015
+ *
+ * Copyright (C) 2015 Justin Brubaker
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * *******************************************
+ */
 
 public class Rename extends JavaPlugin {
-	
+
 	public final Logger logger = Logger.getLogger("Minecraft");
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command command,
 			String commandLabel, String[] args) {
 		Player player = (Player) sender;
-		
-		
-		if (commandLabel.equalsIgnoreCase("rename")) {			
-			if (sender.hasPermission(new Permissions().rename)) {				
-				if (args.length == 1) {				
+
+		if (commandLabel.equalsIgnoreCase("rename")) {
+			if (sender.hasPermission(new Permissions().rename)) {
+				if (args.length == 1) {
 					String nonColoredText = args[0];
 					String coloredText = ChatColor
-							.translateAlternateColorCodes('&', nonColoredText);	
-					
-					PlayerInventory pi = player.getInventory();					
-					ItemStack inHand = pi.getItemInHand();	
-					
-					if (player.getItemInHand().getType() != Material.AIR) {						
-						if (player.getItemInHand().getType() == Material.DIAMOND_PICKAXE) {	
-							
-							ItemStack newitem = new ItemStack(inHand);							
-							ItemMeta im = inHand.getItemMeta();							
-							im.setDisplayName(coloredText);							
-							newitem.setItemMeta(im);							
-							pi.removeItem(inHand);						
-							pi.setItemInHand(newitem);						
+							.translateAlternateColorCodes('&', nonColoredText);
+
+					PlayerInventory pi = player.getInventory();
+					ItemStack inHand = pi.getItemInHand();
+
+					if (player.getItemInHand().getType() != Material.AIR) {
+						if (player.getItemInHand().getType() == Material.DIAMOND_PICKAXE) {
+
+							ItemStack newitem = new ItemStack(inHand);
+							ItemMeta im = inHand.getItemMeta();
+							im.setDisplayName(coloredText);
+							newitem.setItemMeta(im);
+							pi.removeItem(inHand);
+							pi.setItemInHand(newitem);
 							this.logger.info(player.getName()
-									+ ChatColor.translateAlternateColorCodes('&', getConfig().getString("your msg")) + coloredText);							
-							player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("rename complete")));
+									+ ChatColor.translateAlternateColorCodes(
+											'&',
+											getConfig().getString("your msg"))
+									+ coloredText);
+							player.sendMessage(ChatColor
+									.translateAlternateColorCodes(
+											'&',
+											getConfig().getString(
+													"rename complete")));
 						} else {
-							String error4 = getConfig().getString("item in hand is not a diamond pickaxe");
-							player.sendMessage(ChatColor.translateAlternateColorCodes('&', error4));
+							String error4 = getConfig().getString(
+									"item in hand is not a diamond pickaxe");
+							player.sendMessage(ChatColor
+									.translateAlternateColorCodes('&', error4));
 						}
 					} else {
-						String error3 = getConfig().getString("item in hand is air");			
-					    player.sendMessage(ChatColor.translateAlternateColorCodes('&', error3));									
+						String error3 = getConfig().getString(
+								"item in hand is air");
+						player.sendMessage(ChatColor
+								.translateAlternateColorCodes('&', error3));
 					}
 				} else {
-	                   String error2 = getConfig().getString("not enough or too many args");			
-				       player.sendMessage(ChatColor.translateAlternateColorCodes('&', error2));
-						}
+					String error2 = getConfig().getString(
+							"not enough or too many args");
+					player.sendMessage(ChatColor.translateAlternateColorCodes(
+							'&', error2));
+				}
 			} else {
 				String error1 = getConfig().getString("no permission");
-			player.sendMessage(ChatColor.translateAlternateColorCodes('&', error1));
-			}		
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+						error1));
+			}
 		}
 		if (commandLabel.equalsIgnoreCase("renameany")) {
-			if (sender.hasPermission(new Permissions().renameany)) {			
-				if (args.length == 1) {			
-					String nonColoredText2 = args[0];				
+			if (sender.hasPermission(new Permissions().renameany)) {
+				if (args.length == 1) {
+					String nonColoredText2 = args[0];
 					String coloredText2 = ChatColor
-							.translateAlternateColorCodes('&', nonColoredText2);				
-					PlayerInventory pi2 = player.getInventory();			
-					ItemStack inHand2 = pi2.getItemInHand();			
-					if (player.getItemInHand().getType() != Material.AIR) {						
-						ItemStack newitem2 = new ItemStack(inHand2);	
-						ItemMeta im2 = inHand2.getItemMeta();						
-						im2.setDisplayName(coloredText2);						
-						newitem2.setItemMeta(im2);			
-						pi2.removeItem(inHand2);			
-						pi2.setItemInHand(newitem2);						
+							.translateAlternateColorCodes('&', nonColoredText2);
+					PlayerInventory pi2 = player.getInventory();
+					ItemStack inHand2 = pi2.getItemInHand();
+					if (player.getItemInHand().getType() != Material.AIR) {
+						ItemStack newitem2 = new ItemStack(inHand2);
+						ItemMeta im2 = inHand2.getItemMeta();
+						im2.setDisplayName(coloredText2);
+						newitem2.setItemMeta(im2);
+						pi2.removeItem(inHand2);
+						pi2.setItemInHand(newitem2);
 						this.logger.info(player.getName()
-								+ " renamed a(n) item to: " + coloredText2);						
+								+ " renamed a(n) item to: " + coloredText2);
 						player.sendMessage(ChatColor.GREEN + "Item renamed.");
 
-					} else {						
+					} else {
 						player.sendMessage(ChatColor.RED
 								+ "The Item in hand is air.");
 					}
 				} else {
-                   String error2 = getConfig().getString("not_enough_or_too_many_args");
-		
-				
-				  player.sendMessage(ChatColor.translateAlternateColorCodes('&', error2));
+					String error2 = getConfig().getString(
+							"not_enough_or_too_many_args");
+
+					player.sendMessage(ChatColor.translateAlternateColorCodes(
+							'&', error2));
 				}
 			} else {
 				String error1 = getConfig().getString("no_permission");
-		
-				
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', error1));
+
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+						error1));
 			}
-		}		 
-		
+		}
+
 		return false;
 
 	}
@@ -154,11 +161,11 @@ public class Rename extends JavaPlugin {
 				+ pdfFile.getVersion() + " Has Been Enabled.");
 
 		getServer().getPluginManager().addPermission(new Permissions().rename);
-		getServer().getPluginManager().addPermission(new Permissions().renameany);
+		getServer().getPluginManager().addPermission(
+				new Permissions().renameany);
 		getConfig().options().copyDefaults(true);
 		saveConfig();
-				
-		
+
 	}
 
 }
