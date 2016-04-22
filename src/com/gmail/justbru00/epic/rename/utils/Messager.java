8d636f361270ebfe2013cd4c90afd0a4cca8c324
Justin Brubaker <justbru00@gmail.com>
2016-04-22 13:30:53 -0400

    RELEASE VERISON 2.5.3
    Bug fix for problem introduced in last update.

---
+++ src/com/gmail/justbru00/epic/rename/utils/Messager.java
@@ -0,0 +1,59 @@
+/**
+The MIT License (MIT)
+
+Copyright (c) 2016 Justin "JustBru00" Brubaker
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+**/ 
+package com.gmail.justbru00.epic.rename.utils;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.gmail.justbru00.epic.rename.main.RenameRewrite;
+
+
+public class Messager {
+
+	public static String color(String uncolored){
+		String colored = uncolored.replace('_', ' ');
+		return ChatColor.translateAlternateColorCodes('&', colored);		
+	}
+	
+	public static void msgConsole(String msg) {		
+		if (RenameRewrite.clogger != null) {
+		RenameRewrite.clogger.sendMessage(RenameRewrite.Prefix + Messager.color(msg));		
+		} else {
+			RenameRewrite.log.info(ChatColor.stripColor(Messager.color(msg)));
+		}
+	}
+	
+	public static void msgPlayer(String msg, Player player) {
+		player.sendMessage(RenameRewrite.Prefix + Messager.color(msg));
+	}	
+	
+	public static void msgPlayer(Player player, String msg) {
+		player.sendMessage(RenameRewrite.Prefix + Messager.color(msg));
+	}	
+	
+	public static void msgSender(String msg, CommandSender sender) {
+		sender.sendMessage(RenameRewrite.Prefix + Messager.color(msg));
+	}	
+}
