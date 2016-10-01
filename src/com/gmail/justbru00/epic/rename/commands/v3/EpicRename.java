31d961d7d6058b0db4f533da59b87e0d3e25a047
Justin Brubaker <justbru00@gmail.com>
2016-09-30 21:11:26 -0400

    Working towards version 3.0
    Added /epicrename command code. #20
    Added v3_messages.yml for the version. #33 (Does not generate or get
    read from)
    Updated plugin.yml to reflect that we are working towards version 3.0

--- src/com/gmail/justbru00/epic/rename/commands/v3/EpicRename.java
+++ src/com/gmail/justbru00/epic/rename/commands/v3/EpicRename.java
@@ -4,13 +4,68 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import com.gmail.justbru00.epic.rename.main.v3.Main_NewRenameRewrite;
+import com.gmail.justbru00.epic.rename.utils.Messager;
+
 public class EpicRename implements CommandExecutor {
 	
 	// VERSION 3
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Start Command Handling | EpicRename
+		if (command.getName().equalsIgnoreCase("epicrename")) {
+			
+			if (!sender.hasPermission("epicrename.epicrename")) { // Check Basic Permission
+				Messager.msgSender(Main_NewRenameRewrite.getMsgFromConfig("epicrename.no_permission"), sender);
+				return true;
+			}
+			
+			if (args.length > 0) {
+				if (args[0].equalsIgnoreCase("license")) { // Start /epicrename license
+					Messager.msgSender(Main_NewRenameRewrite.getMsgFromConfig("epicrename.license"), sender);
+					return true;
+				} else if (args[0].equalsIgnoreCase("help")) { // End /epicrename license | Start /epicrename help
+					Messager.msgSender(Main_NewRenameRewrite.getMsgFromConfig("epicrename.help"), sender);
+					return true;
+				} else if (args[0].equalsIgnoreCase("reload")) { // End /epicrename license | Start /epicrename reload
+					if (sender.hasPermission("epicrename.reload")) {
+						Main_NewRenameRewrite.reloadConfigs();
+						Messager.msgSender(Main_NewRenameRewrite.getMsgFromConfig("epicrename.reload_success"), sender);
+						return true;
+					} else {
+						Messager.msgSender(Main_NewRenameRewrite.getMsgFromConfig("epicrename.no_permission"), sender);
+						return true;
+					}					
+				} else if (args[0].equalsIgnoreCase("debug")) { // End /epicrename reload | Start /epicrename debug
+					if (sender.hasPermission("epicrename.epicrename.debug")) {
+						if (Main_NewRenameRewrite.debug) {
+							Main_NewRenameRewrite.debug = false;
+							Messager.msgSender(Main_NewRenameRewrite.getMsgFromConfig("epicrename.debug_disable"), sender);
+							return true;
+						} else {
+							Main_NewRenameRewrite.debug = true;
+							Messager.msgSender(Main_NewRenameRewrite.getMsgFromConfig("epicrename.debug_enable"), sender);
+							return true;
+						}
+					} else {
+						Messager.msgSender(Main_NewRenameRewrite.getMsgFromConfig("epicrename.no_permission"), sender);
+						return true;
+					}
+				} else if (args[0].equalsIgnoreCase("version")) { // End /epicrename debug | Start /epicrename version
+					Messager.msgSender(Main_NewRenameRewrite.getMsgFromConfig("epicrename.version"), sender); // TODO Replace {version} varible.
+					return true;
+				} else { // End /epicrename version | Start /epicrename invalid args
+					Messager.msgSender(Main_NewRenameRewrite.getMsgFromConfig("epicrename.invalid_args"), sender);
+					return true;
+				}
+			} else { // No Args				
+				Messager.msgSender(Main_NewRenameRewrite.getMsgFromConfig("epicrename.no_args"), sender);				
+				return true;
+			} //End No args
+		} // End Command Handling | EpicRename
+		
 		return false;
 	}
 
---
+++ src/com/gmail/justbru00/epic/rename/main/v3/Main_NewRenameRewrite.java
@@ -0,0 +1,23 @@
+package com.gmail.justbru00.epic.rename.main.v3;
+
+public class Main_NewRenameRewrite /** TODO Extend Java plugin**/{
+	
+	public static boolean debug = false;
+	public static String PLUGIN_VERISON; // TODO Make final and get version from plugin.yml
+
+	
+	/**
+	 * 
+	 * @param path Path to the message in messages.yml
+	 * @return The colored string from messages.yml
+	 */
+	public static String getMsgFromConfig(String path) {
+		// TODO Get string from config and color.
+		return null;
+	}
+	
+	public static void reloadConfigs() {
+		// TODO Reload config.yml and messages.yml
+	}
+	
+}
