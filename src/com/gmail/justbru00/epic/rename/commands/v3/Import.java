ad0dcabbe74bc244b6b6b74ef453efc0cb11d435
JustBru00 <justbru00@gmail.com>
2019-05-26 14:57:59 -0400

    Worked on #106

--- src/com/gmail/justbru00/epic/rename/commands/v3/Import.java
+++ src/com/gmail/justbru00/epic/rename/commands/v3/Import.java
@@ -5,96 +5,177 @@
  */
 package com.gmail.justbru00.epic.rename.commands.v3;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
+import com.gmail.justbru00.epic.rename.main.v3.Main;
+import com.gmail.justbru00.epic.rename.utils.v3.Debug;
 import com.gmail.justbru00.epic.rename.utils.v3.ItemSerialization;
 import com.gmail.justbru00.epic.rename.utils.v3.Messager;
+import com.gmail.justbru00.epic.rename.utils.v3.PasteBinAPI;
+
 /**
  * Created for #106
+ * 
  * @author Justin Brubaker
  *
  */
 public class Import implements CommandExecutor {
-	
+
+	private static ArrayList<UUID> playersWhoHaveConfirmed = new ArrayList<UUID>();
+
 	/**
 	 * Usage:
 	 * 
-	 *  import <hand,inventory> <link,rawyaml>
-	 *  
-	 *  Examples:
-	 *  
-	 *  import hand https://pastebin.com/ValidLink
-	 *  import hand random yaml stuff
-	 *  import inventory https://pastebin.com/ValidLink
-	 *  import inv https://pastebin.com/ValidLink
-	 *  import h https://pastebin.com/ValidLink
-	 *  import i https://pastebin.com/ValidLink
-	 *  import raw yaml text here
-	 *  
-	 *  Importing an inventory forces a /import confirm before allowing a player to import once per server restart.
-	 *  Importing to your hand only works if the hand is empty.
+	 * import <hand,inventory> <link,rawyaml>
+	 * 
+	 * Examples:
+	 * 
+	 * import hand https://pastebin.com/ValidLink import hand random yaml stuff
+	 * import inventory https://pastebin.com/ValidLink import inv
+	 * https://pastebin.com/ValidLink import h https://pastebin.com/ValidLink import
+	 * i https://pastebin.com/ValidLink import raw yaml text here
+	 * 
+	 * Importing an inventory forces a /import confirm before allowing a player to
+	 * import once per server restart. Importing to your hand only works if the hand
+	 * is empty.
 	 *
 	 **/
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		
+
 		if (sender instanceof Player) {
-			Player p = (Player) sender;			
-			
+			Player p = (Player) sender;
+
 			if (sender.hasPermission("epicrename.import")) {
-				
+
 				if (args.length != 0) {
 					PlayerInventory inv = p.getInventory();
 					ItemStack mainHand = inv.getItemInMainHand();
 					if (args[0].equalsIgnoreCase("hand") || args[0].equalsIgnoreCase("h")) {
 						// /import hand
-						
-						
+						if (args.length == 2) {
+
+							if (mainHand == null || mainHand.getType() == Material.AIR) {
+
+								String link = args[1];
+								String textFromWeb = null;
+
+								try {
+									textFromWeb = PasteBinAPI.getTextFromURL(link).get();
+								} catch (IOException e) {
+									Debug.send("Failed to GET text data from URL.");
+									Messager.msgSender(Main.getMsgFromConfig("import.failed_to_get_data")
+											.replace("{link}", link).replace("{error}", e.getMessage()), sender);
+									return true;
+								} catch (NoSuchElementException e2) {
+									Messager.msgSender(Main.getMsgFromConfig("import.failed_to_get_data")
+											.replace("{link}", link).replace("{error}", "Text is null."), sender);
+									return true;
+								}
+
+								ItemStack imported = ItemSerialization.toItem(textFromWeb);
+
+								if (imported == null) {
+									// FAILED
+									Messager.msgSenderWithConfigMsg("import.yaml_fail", sender);
+									return true;
+								}
+
+								inv.setItemInMainHand(imported);
+								Messager.msgSenderWithConfigMsg("import.success", sender);
+								return true;
+							} else {
+								Messager.msgSenderWithConfigMsg("import.full_hand", sender);
+								return true;
+							}
+						} else {
+							Messager.msgSenderWithConfigMsg("import.wrong_args_hand", sender);
+							return true;
+						}
 					} else if (args[0].equalsIgnoreCase("inventory") || args[0].equalsIgnoreCase("inv")
-							|| args[0].equalsIgnoreCase("i")) {						
-						// /import inventory 
-						
-						
+							|| args[0].equalsIgnoreCase("i")) {
+						// /import inventory
+
+						// Confirmation
+						if (!playersWhoHaveConfirmed.contains(p.getUniqueId())) {
+							Messager.msgSenderWithConfigMsg("import.not_confirmed", sender);
+							return true;
+						}
+
+						if (args.length == 2) {
+
+							String link = args[1];
+							String textFromWeb = null;
+
+							try {
+								textFromWeb = PasteBinAPI.getTextFromURL(link).get();
+							} catch (IOException e) {
+								Debug.send("Failed to GET text data from URL.");
+								Messager.msgSender(Main.getMsgFromConfig("import.failed_to_get_data")
+										.replace("{link}", link).replace("{error}", e.getMessage()), sender);
+								return true;
+							} catch (NoSuchElementException e2) {
+								Messager.msgSender(Main.getMsgFromConfig("import.failed_to_get_data")
+										.replace("{link}", link).replace("{error}", "Text is null."), sender);
+								return true;
+							}
+
+							ItemSerialization.fillInventoryFromString(textFromWeb, p);
+
+							Messager.msgSenderWithConfigMsg("import.success", sender);
+							return true;
+						} else {
+							Messager.msgSenderWithConfigMsg("import.wrong_args_inventory", sender);
+							return true;
+						}
 					} else if (args[0].equalsIgnoreCase("raw") || args[0].equalsIgnoreCase("r")) {
 						// /import raw
-						
+
 						// Try to import the raw yaml text. Single item only.
 						if (mainHand == null || mainHand.getType() == Material.AIR) {
 							// Empty Main Hand
 							StringBuilder yamlDataBuilder = new StringBuilder("");
-							
+
 							for (int i = 1; i < args.length; i++) {
 								yamlDataBuilder.append(args[i] + " ");
 							}
-							
+
 							String yamlData = yamlDataBuilder.toString().trim();
-							
+
 							ItemStack importedItem = ItemSerialization.toItem(yamlData);
-							
+
 							if (importedItem == null) {
 								// FAILED
-								Messager.msgSenderWithConfigMsg("import.raw_fail", sender);
+								Messager.msgSenderWithConfigMsg("import.yaml_fail", sender);
 								return true;
 							}
-							
+
 							// Put ItemStack in the players main hand.
-							
+
 							inv.setItemInMainHand(importedItem);
-							Messager.msgSenderWithConfigMsg("import.success", sender);									
+							Messager.msgSenderWithConfigMsg("import.success", sender);
 							return true;
 						} else {
 							// Must have empty hand
 							Messager.msgSenderWithConfigMsg("import.full_hand", sender);
 							return true;
 						}
+					} else if (args[0].equalsIgnoreCase("confirm")) {
+						playersWhoHaveConfirmed.add(p.getUniqueId());
+						Messager.msgSenderWithConfigMsg("import.confirmed", sender);
+						return true;
 					} else {
 						Messager.msgSenderWithConfigMsg("import.wrong_args", sender);
 						return true;
@@ -111,8 +192,6 @@ public class Import implements CommandExecutor {
 			Messager.msgSenderWithConfigMsg("import.wrong_sender", sender);
 			return true;
 		}
-		
-		return true;
 	}
 
 }
--- src/com/gmail/justbru00/epic/rename/utils/v3/PasteBinAPI.java
+++ src/com/gmail/justbru00/epic/rename/utils/v3/PasteBinAPI.java
@@ -9,8 +9,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Optional;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -18,31 +20,87 @@ import com.gmail.justbru00.epic.rename.main.v3.Main;
 
 /**
  * Created for issue #105, #106
+ * 
  * @author Justin Brubaker
  *
  */
 public class PasteBinAPI {
-	
+
 	private static final String DEV_KEY = "c577908005909793b051b84c01254a82";
 	private static final String POST_URL = "https://pastebin.com/api/api_post.php";
-	private static final String POST_PARAMETERS = "api_option=paste&api_dev_key=" + DEV_KEY + "&api_paste_name=EpicRename v" + Main.PLUGIN_VERISON + " /export&api_paste_code={DATA}&api_paste_format=yaml&api_paste_expire_date=1M";
-	
+	private static final String POST_PARAMETERS = "api_option=paste&api_dev_key=" + DEV_KEY
+			+ "&api_paste_name=EpicRename v" + Main.PLUGIN_VERISON
+			+ " /export&api_paste_code={DATA}&api_paste_format=yaml&api_paste_expire_date=1M";
+
+	/**
+	 * Attempts to GET raw text from a URL. If the URL is a pastebin link such as
+	 * https://pastebin.com/e5mwvrJ8 then the method will convert that link to
+	 * https://pastebin.com/raw/e5mwvrJ8 If the URL is not a pastebin link then it
+	 * will just attempt to get raw text.
+	 * 
+	 * @param url
+	 * @return The text retrieved from the server
+	 * @throws IOException
+	 */
+	public static Optional<String> getTextFromURL(String url) throws IOException {
+		if (url.contains("https://pastebin.com/") && !url.contains("raw/")) {
+			String newUrl = "";
+
+			newUrl = url.substring(0, 22);
+
+			newUrl = "https://pastebin.com/raw/VALUE".replace("VALUE", newUrl);
+			url = newUrl;
+		}
+
+		URL urlObj = null;
+		String textData = null;
+
+		urlObj = new URL(url);
+
+		HttpURLConnection httpConn = (HttpURLConnection) urlObj.openConnection();
+		httpConn.setRequestProperty("Content-Type", "0");
+		httpConn.setRequestMethod("GET");
+		httpConn.setUseCaches(false);
+		httpConn.setConnectTimeout(10000);
+		httpConn.setReadTimeout(10000);
+		httpConn.setAllowUserInteraction(false);
+		httpConn.setInstanceFollowRedirects(true);
+		httpConn.setRequestProperty("Connection", "close");
+		httpConn.connect();
+
+		// Attempt to get the json data
+		BufferedReader in = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		textData = response.toString();
+
+		return Optional.ofNullable(textData);
+	}
+
 	/**
 	 * Pastes the data provided directly to https://pastebin.com/
+	 * 
 	 * @param data The text to paste.
-	 * @return The response from pastebin. This can be a link to the paste or an error message beginning with 
-	 * "Bad API request," 
-	 * @throws IOException 
-	 * @throws MalformedURLException 
+	 * @return The response from pastebin. This can be a link to the paste or an
+	 *         error message beginning with "Bad API request,"
+	 * @throws IOException
+	 * @throws MalformedURLException
 	 */
 	public static String paste(String data) throws MalformedURLException, IOException {
 		String response = post(data);
-		
+
 		return response;
 	}
-	
+
 	/**
 	 * Posts text data to PasteBin.
+	 * 
 	 * @param data
 	 * @return If this contains "Bad API request," then the post failed.
 	 * @throws IOException
@@ -51,44 +109,41 @@ public class PasteBinAPI {
 	private static String post(String data) throws IOException, MalformedURLException {
 		URL formattedUrl = null;
 		formattedUrl = new URL(POST_URL);
-		
+
 		String formattedData = POST_PARAMETERS.replace("{DATA}", data);
-		
-		
-		Debug.send("[PasteBinAPI] Attempting to POST.");		
-		
-			HttpsURLConnection httpsCon = (HttpsURLConnection) formattedUrl.openConnection();
-			httpsCon.setDoOutput(true);
-			httpsCon.setDoInput(true);
-			httpsCon.setRequestMethod("POST");
-			
-			OutputStreamWriter out = new OutputStreamWriter(
-			    httpsCon.getOutputStream());
-			out.write(formattedData);
-			out.flush();
-			out.close();
-			
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					httpsCon.getInputStream()));
-			StringBuilder builder = new StringBuilder();
-			String line;
-			while ((line = reader.readLine()) != null) {
-				if (builder.length() > 0) {
-					builder.append('\n');
-				}
-				builder.append(line);
+
+		Debug.send("[PasteBinAPI] Attempting to POST.");
+
+		HttpsURLConnection httpsCon = (HttpsURLConnection) formattedUrl.openConnection();
+		httpsCon.setDoOutput(true);
+		httpsCon.setDoInput(true);
+		httpsCon.setRequestMethod("POST");
+
+		OutputStreamWriter out = new OutputStreamWriter(httpsCon.getOutputStream());
+		out.write(formattedData);
+		out.flush();
+		out.close();
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(httpsCon.getInputStream()));
+		StringBuilder builder = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			if (builder.length() > 0) {
+				builder.append('\n');
 			}
-			reader.close();
-			
-			String response = builder.toString();
-					
-			Debug.send("[PasteBinAPI] POST response code was: " + httpsCon.getResponseCode());
-			
-			if (response.contains("Bad API request,"))	{
-				Debug.send("[PasteBinAPI] FAILED TO POST: " + response);
-			}
-			
-			return response;
+			builder.append(line);
+		}
+		reader.close();
+
+		String response = builder.toString();
+
+		Debug.send("[PasteBinAPI] POST response code was: " + httpsCon.getResponseCode());
+
+		if (response.contains("Bad API request,")) {
+			Debug.send("[PasteBinAPI] FAILED TO POST: " + response);
+		}
+
+		return response;
 	}
-	
+
 }
